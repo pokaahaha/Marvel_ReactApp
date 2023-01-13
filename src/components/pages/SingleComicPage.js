6dff480153c2187b07c85af68fb01a2d06b81c19
pokaahaha <men101@protonmail.com>
2023-01-13 19:23:57 +0300

    add dynamic pages v2

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import useMarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
+import AppBanner from "../appBanner/AppBanner";
 import "./singleComicPage.scss";
 
 const SingleComicPage = () => {
@@ -26,10 +27,11 @@ const SingleComicPage = () => {
 
   const errorMessage = error ? <ErrorMessage /> : null;
   const spinner = loading ? <Spinner /> : null;
-  const content = !(loading || error || !comic) ? <View char={comic} /> : null;
+  const content = !(loading || error || !comic) ? <View comic={comic} /> : null;
 
   return (
     <>
+      <AppBanner />
       {errorMessage}
       {spinner}
       {content}
@@ -39,6 +41,7 @@ const SingleComicPage = () => {
 
 const View = ({ comic }) => {
   const { title, description, pageCount, thumbnail, language, price } = comic;
+
   return (
     <div className="single-comic">
       <img src={thumbnail} alt={title} className="single-comic__img" />
@@ -47,7 +50,7 @@ const View = ({ comic }) => {
         <p className="single-comic__descr">{description}</p>
         <p className="single-comic__descr">{pageCount}</p>
         <p className="single-comic__descr">Language: {language}</p>
-        <div className="single-comic__price">{price}$</div>
+        <div className="single-comic__price">{price}</div>
       </div>
       <Link to="/comics" className="single-comic__back">
         Back to all
